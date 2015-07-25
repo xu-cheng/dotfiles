019ba7e9fdf2fd1881604d7e2827fb292c84e601
Xu Cheng <xucheng@me.com>
2015-07-25 19:00:00 +0800

    brew build-log: handle case when formula is not found

--- home/bin/brew-build-log.rb
+++ home/bin/brew-build-log.rb
@@ -1,7 +1,7 @@
 fpp = which("fpp")
 abort "Please install fpp with `brew instal fpp`." unless fpp
 
-logd = ARGV.resolved_formulae.first.logs
+logd = ARGV.resolved_formulae.first.logs rescue HOMEBREW_LOGS/ARGV.named.first
 abort "#{logd} doesn't exist." unless logd.directory?
 
 exec "find #{logd} | #{fpp}"
