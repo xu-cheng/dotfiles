0d5cee1d5369d8a9f73a106e42b610663a618219
Xu Cheng <git@xuc.me>
2018-03-12 18:45:27 +0800

    xdg: add cocoapods

--- home/.config/zsh/custom/plugins/xdg-base-dir/xdg-base-dir.plugin.zsh
+++ home/.config/zsh/custom/plugins/xdg-base-dir/xdg-base-dir.plugin.zsh
@@ -9,6 +9,12 @@ export BUNDLE_USER_CACHE="$XDG_CACHE_HOME/bundle"
 # https://github.com/chrisallenlane/cheat#setting-a-default_cheat_dir
 export DEFAULT_CHEAT_DIR="$XDG_CONFIG_HOME/cheat"
 
+# cocoapods
+# https://github.com/CocoaPods/CocoaPods/blob/master/lib/cocoapods/config.rb
+export CP_HOME_DIR="$XDG_CONFIG_HOME/cocoapods"
+export CP_REPOS_DIR="$XDG_CACHE_HOME/cocoapods/repos"
+export CP_TEMPLATES_DIR="$XDG_DATA_HOME/cocoapods/templates"
+
 # cpanm
 # https://github.com/miyagawa/cpanminus#how-does-cpanm-getparseupdate-the-cpan-index
 export PERL_CPANM_HOME="$XDG_CACHE_HOME/cpanm"
