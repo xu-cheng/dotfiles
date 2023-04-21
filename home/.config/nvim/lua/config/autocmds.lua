93c1840b0ceed77af1c0cef4116c63750bfcf5d5
Cheng XU <git@xuc.me>
2023-04-20 18:46:32 -0700

    nvim autocmd: set cursor to the first line when editing a git commit message

--- home/.config/nvim/lua/config/autocmds.lua
+++ home/.config/nvim/lua/config/autocmds.lua
@@ -48,11 +48,17 @@ vim.api.nvim_create_autocmd("BufEnter", {
 vim.api.nvim_create_autocmd("BufReadPost", {
     desc = "Go to last loc when opening a buffer",
     group = augroup("last_loc"),
-    callback = function()
-        local mark = vim.api.nvim_buf_get_mark(0, '"')
-        local lcount = vim.api.nvim_buf_line_count(0)
-        if mark[1] > 0 and mark[1] <= lcount then
-            pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    callback = function(event)
+        -- set cursor to the first line when editing a git commit message
+        if vim.bo.filetype == "gitcommit" and
+           vim.fn.fnamemodify(event.file, ":t") == "COMMIT_EDITMSG" then
+            pcall(vim.api.nvim_win_set_cursor, 0, {1, 0})
+        else
+            local mark = vim.api.nvim_buf_get_mark(0, '"')
+            local lcount = vim.api.nvim_buf_line_count(0)
+            if mark[1] > 0 and mark[1] <= lcount then
+                pcall(vim.api.nvim_win_set_cursor, 0, mark)
+            end
         end
     end
 })
