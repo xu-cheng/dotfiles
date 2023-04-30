336cfd8b1cc83262df6741e9ce78dab87e24ccd2
Cheng XU <git@xuc.me>
2023-04-29 19:22:10 -0700

    nvim: add gitsign

--- home/.config/nvim/lua/plugins/editor.lua
+++ home/.config/nvim/lua/plugins/editor.lua
@@ -15,4 +15,44 @@ return {
         config = true,
     },
 
+    -- git
+    {
+        "lewis6991/gitsigns.nvim",
+        enabled = not_vscode,
+        event = "VeryLazy",
+        opts = {
+            signs = {
+                add = { text = "▎" },
+                change = { text = "▎" },
+                delete = { text = "" },
+                topdelete = { text = "" },
+                changedelete = { text = "▎" },
+                untracked = { text = "▎" },
+            },
+            on_attach = function(buffer)
+                local gs = package.loaded.gitsigns
+
+                local function map(mode, l, r, desc)
+                    vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
+                end
+
+                map("n", "]h", gs.next_hunk, "Next Hunk")
+                map("n", "[h", gs.prev_hunk, "Prev Hunk")
+                map({ "n", "v" }, "<leader>gs", ":Gitsigns stage_hunk<CR>", "Stage Hunk")
+                map({ "n", "v" }, "<leader>gr", ":Gitsigns reset_hunk<CR>", "Reset Hunk")
+                map("n", "<leader>gS", gs.stage_buffer, "Stage Buffer")
+                map("n", "<leader>gu", gs.undo_stage_hunk, "Undo Stage Hunk")
+                map("n", "<leader>gR", gs.reset_buffer, "Reset Buffer")
+                map("n", "<leader>gp", gs.preview_hunk, "Preview Hunk")
+                map("n", "<leader>gb", function() gs.blame_line({ full = true }) end, "Blame Line")
+                map("n", "<leader>gd", gs.diffthis, "Diff This")
+                map("n", "<leader>gD", function() gs.diffthis("~") end, "Diff This ~")
+                map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "GitSigns Select Hunk")
+            end,
+        },
+        main = "gitsigns",
+        config = true,
+    }
+
+
 }
