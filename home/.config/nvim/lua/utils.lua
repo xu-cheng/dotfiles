863f5624fe8042044c1e3edd36496886a1d94a5a
Cheng XU <git@xuc.me>
2023-04-29 21:02:12 -0700

    update utils.lua

--- home/.config/nvim/lua/utils.lua
+++ home/.config/nvim/lua/utils.lua
@@ -1,10 +1,10 @@
 local Utils = {}
 
-Utils.augroup = function(name)
+function Utils.augroup (name)
     return vim.api.nvim_create_augroup(name, { clear = true })
 end
 
-Utils.fg = function(name)
+function Utils.fg (name)
     local hl = vim.api.nvim_get_hl(0, { name = name })
     local fg = hl and hl.fg
     return fg and string.format("#%06x", fg)
