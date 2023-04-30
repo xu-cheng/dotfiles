5fd1e220c2bcc2bc8d3262baf438f4dc6adf62d2
Cheng XU <git@xuc.me>
2023-04-29 23:04:29 -0700

    update nvim

--- home/.config/nvim/lua/plugins/utils.lua
+++ home/.config/nvim/lua/plugins/utils.lua
@@ -1,3 +1,20 @@
 return {
-    "dstein64/vim-startuptime",
+     -- measure startuptime
+    {
+        "dstein64/vim-startuptime",
+        cmd = "StartupTime",
+    },
+
+    -- library used by other plugins
+    {
+        "nvim-lua/plenary.nvim",
+        lazy = true,
+    },
+
+    -- makes some plugins dot-repeatable like leap
+    {
+        "tpope/vim-repeat",
+        version = false,
+        event = "VeryLazy"
+    },
 }
