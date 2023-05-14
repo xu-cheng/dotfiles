abce9cd70b13b20318d120657b85dafcc7fd03a8
Cheng XU <git@xuc.me>
2023-05-13 20:34:19 -0700

    nvim: add dressing.nvim

--- home/.config/nvim/lua/plugins/ui.lua
+++ home/.config/nvim/lua/plugins/ui.lua
@@ -115,6 +115,22 @@ return {
             },
         },
     },
+    {
+        "stevearc/dressing.nvim",
+        version = false,
+        enabled = not_vscode,
+        lazy = true,
+        init = function()
+            vim.ui.select = function(...)
+                require("lazy").load({ plugins = { "dressing.nvim" } })
+                return vim.ui.select(...)
+            end
+            vim.ui.input = function(...)
+                require("lazy").load({ plugins = { "dressing.nvim" } })
+                return vim.ui.input(...)
+            end
+        end,
+    },
 
     -- bufferline
     {
