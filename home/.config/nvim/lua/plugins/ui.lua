aa62131802f59dffd69b72705bfcc84ad7d1820d
Cheng XU <git@xuc.me>
2023-05-13 21:08:35 -0700

    nvim dashboard: update keymapping

--- home/.config/nvim/lua/plugins/ui.lua
+++ home/.config/nvim/lua/plugins/ui.lua
@@ -291,7 +291,7 @@ return {
             local dashboard = require("alpha.themes.dashboard")
             dashboard.section.buttons.val = {
                 dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
-                dashboard.button("n", " " .. " New file", ":ene <BAR> startinsert <CR>"),
+                dashboard.button("i", " " .. " New file", ":ene <BAR> startinsert <CR>"),
                 dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
                 dashboard.button("g", " " .. " Find text", ":Telescope live_grep <CR>"),
                 dashboard.button("s", " " .. " Restore Session", [[:lua require("persistence").load() <cr>]]),
