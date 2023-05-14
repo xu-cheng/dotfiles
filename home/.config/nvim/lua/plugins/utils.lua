8cc155889141416143c8439140d0e88c64700d47
Cheng XU <git@xuc.me>
2023-05-13 20:27:31 -0700

    nvim: add session managment

--- home/.config/nvim/lua/plugins/utils.lua
+++ home/.config/nvim/lua/plugins/utils.lua
@@ -1,8 +1,22 @@
 return {
-     -- measure startuptime
+    -- measure startuptime
     {
         "dstein64/vim-startuptime",
         cmd = "StartupTime",
+    },
+
+    -- session management
+    {
+        "folke/persistence.nvim",
+        version = false,
+        enabled = not vim.g.vscode,
+        event = "BufReadPre",
+        opts = { options = { "buffers", "curdir", "tabpages", "winsize", "help", "globals" } },
+        keys = {
+            { "<leader>qs", function() require("persistence").load() end, desc = "Restore Session" },
+            { "<leader>ql", function() require("persistence").load({ last = true }) end, desc = "Restore Last Session" },
+            { "<leader>qd", function() require("persistence").stop() end, desc = "Don't Save Current Session" },
+        },
     },
 
     -- library used by other plugins
