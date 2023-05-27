45abcd2fbd641801be5e8eb6b5747e55704e49d5
Cheng XU <git@xuc.me>
2023-05-27 02:08:50 -0700

    nvim: remove dup config

--- home/.config/nvim/lua/plugins/lsp.lua
+++ home/.config/nvim/lua/plugins/lsp.lua
@@ -45,9 +45,6 @@ return {
             local luasnip = require("luasnip")
 
             cmp.setup({
-                completion = {
-                    completeopt = "menu,menuone,noinsert",
-                },
                 snippet = {
                     expand = function(args)
                         require('luasnip').lsp_expand(args.body)
