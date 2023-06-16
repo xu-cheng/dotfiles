ab08114198f1c21a9ad085ac0bcbe536231e73d0
Cheng XU <git@xuc.me>
2023-06-15 20:17:51 -0700

    nvim: update commentstring setting for mini.comment

--- home/.config/nvim/lua/plugins/editing.lua
+++ home/.config/nvim/lua/plugins/editing.lua
@@ -5,9 +5,9 @@ return {
         version = false,
         event = "VeryLazy",
         opts = {
-            hooks = {
-                pre = function()
-                    require("ts_context_commentstring.internal").update_commentstring({})
+            options = {
+                custom_commentstring = function()
+                    return require("ts_context_commentstring.internal").calculate_commentstring() or vim.bo.commentstring
                 end,
             },
         },
