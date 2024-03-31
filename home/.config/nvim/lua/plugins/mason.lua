b32d9004daefa27add4b1121c9431f860d2d3b79
Cheng XU <git@xuc.me>
2024-03-31 16:35:29 -0700

    efm: add editorconfig-checker

--- home/.config/nvim/lua/plugins/mason.lua
+++ home/.config/nvim/lua/plugins/mason.lua
@@ -37,6 +37,7 @@ return {
             linux_ensure_installed = {
                 "bash-language-server",
                 "css-lsp",
+                "editorconfig-checker",
                 "efm",
                 "eslint-lsp",
                 "html-lsp",
