5524bb84dfc5f47d20d0c927182d89f37b529ae4
Cheng Xu <git@xuc.me>
2025-05-26 20:11:23 -0700

    fmt

--- home/.config/nvim/lua/plugins/markdown.lua
+++ home/.config/nvim/lua/plugins/markdown.lua
@@ -5,7 +5,10 @@ return {
         "MeanderingProgrammer/render-markdown.nvim",
         enabled = not_vscode,
         ft = { "markdown" },
-        dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter",
+            "nvim-tree/nvim-web-devicons",
+        },
         ---@module "render-markdown"
         ---@type render.md.UserConfig
         opts = {
