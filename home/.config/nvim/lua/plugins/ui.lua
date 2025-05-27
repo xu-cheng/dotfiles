20afa17dd7e5964a1ab6ea00376e693e8b8e1e29
Cheng Xu <git@xuc.me>
2025-05-26 17:41:10 -0700

    rm remaining reference to cmp

--- home/.config/nvim/lua/plugins/ui.lua
+++ home/.config/nvim/lua/plugins/ui.lua
@@ -116,11 +116,10 @@ return {
         },
         opts = {
             lsp = {
-                -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+                -- override markdown rendering so that plugins use **Treesitter**
                 override = {
                     ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
                     ["vim.lsp.util.stylize_markdown"] = true,
-                    ["cmp.entry.get_documentation"] = true,
                 },
             },
             presets = {
