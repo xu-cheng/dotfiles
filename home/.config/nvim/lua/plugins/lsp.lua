870eb28b2ce6aba483581fa992d4e2a0cbac5437
Cheng Xu <git@xuc.me>
2026-01-16 13:25:45 -0800

    nvim: fix enter autocomplete

--- home/.config/nvim/lua/plugins/lsp.lua
+++ home/.config/nvim/lua/plugins/lsp.lua
@@ -22,7 +22,7 @@ return {
         opts = {
             keymap = {
                 preset = "super-tab",
-                ["<CR>"] = { "accept", "fallback" },
+                ["<CR>"] = { "select_and_accept", "fallback" },
                 ["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
                 ["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
             },
