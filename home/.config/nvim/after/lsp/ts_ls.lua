d4143d9cc31576af7abff3c73b8910ecee010952
Cheng Xu <git@xuc.me>
2026-02-03 10:40:54 -0800

    update comment

--- home/.config/nvim/after/lsp/ts_ls.lua
+++ home/.config/nvim/after/lsp/ts_ls.lua
@@ -17,7 +17,7 @@ return {
         typescript = settings,
     },
     on_attach = function(client)
-        -- we use efm for formatting
+        -- we use biome for formatting
         if client.name == "ts_ls" then
             client.server_capabilities.documentFormattingProvider = false
             client.server_capabilities.documentRangeFormattingProvider = false
