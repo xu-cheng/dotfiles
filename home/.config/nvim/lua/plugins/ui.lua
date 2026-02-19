10d74e9d5c05394bc8dbb99499e0e4cf7ec541a7
Cheng Xu <git@xuc.me>
2026-02-19 12:51:17 -0800

    nvim: update noice cfg

--- home/.config/nvim/lua/plugins/ui.lua
+++ home/.config/nvim/lua/plugins/ui.lua
@@ -123,11 +123,12 @@ return {
                 },
             },
             presets = {
-                bottom_search = true,         -- use a classic bottom cmdline for search
-                command_palette = true,       -- position the cmdline and popupmenu together
-                long_message_to_split = true, -- long messages will be sent to a split
-                inc_rename = true,            -- enables an input dialog for inc-rename.nvim
-                lsp_doc_border = false,       -- add a border to hover docs and signature help
+                bottom_search = true,           -- use a classic bottom cmdline for search
+                command_palette = true,         -- position the cmdline and popupmenu together
+                long_message_to_split = true,   -- long messages will be sent to a split
+                inc_rename = true,              -- enables an input dialog for inc-rename.nvim
+                lsp_doc_border = false,         -- add a border to hover docs and signature help
+                cmdline_output_to_split = true, -- send the output of a command you executed in the cmdline to a split
             },
             cmdline = {
                 format = {
@@ -136,6 +137,11 @@ return {
                 },
             },
             routes = {
+                -- show @recording messages
+                {
+                    view = "notify",
+                    filter = { event = "msg_showmode" },
+                },
                 -- stop builtin spelling suggestion view, use which-key instead
                 {
                     filter = { event = "msg_show", find = "Type number and <Enter> or click with the mouse" },
